f3cd5ec5b75ab03894a57d654d2ba721b76894ba
Jonathan Marois <jnt.marois@gmail.com>
2022-08-19 13:03:22 +0200

    _

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -5,7 +5,7 @@ import UpdatePostValidator from 'App/Validators/Posts/UpdatePostValidator'
 import Application from '@ioc:Adonis/Core/Application'
 
 export default class PostsController {
-  public async index({response }: HttpContextContract) {
+  public async index({ response }: HttpContextContract) {
 
     const posts = await Post
       .query()
@@ -14,55 +14,54 @@ export default class PostsController {
 
     return response.ok(posts)
   }
-  public async likes({request, response }: HttpContextContract) {
-    const payload =request.body()
+  public async likes({ request, response }: HttpContextContract) {
+    const payload = request.body()
     const post = await Post.findOrFail(payload.post_id)
     const liked_by = post.$attributes.liked_by
-    let unpackArr = JSON.parse( liked_by )
+    let unpackArr = JSON.parse(liked_by)
 
-      unpackArr  == null ?? []
+    unpackArr == null ?? []
 
 
-    if(unpackArr != null && unpackArr.includes(parseInt(payload.user_id))){
+    if (unpackArr != null && unpackArr.includes(parseInt(payload.user_id))) {
       let index = unpackArr.indexOf(parseInt(payload.user_id))
       unpackArr.splice(index)
     }
-    else{
-     unpackArr.push(parseInt(payload.user_id))
+    else {
+      unpackArr.push(parseInt(payload.user_id))
     }
     let mergeable = {}
     let json = JSON.stringify(unpackArr)
-    Object.assign(mergeable, {liked_by: json})
+    Object.assign(mergeable, { liked_by: json })
     post.merge(mergeable).save()
     return response.ok(post)
   }
 
   public async store({ request, response }: HttpContextContract) {
-    let url =""
+    let url = ""
     const file = request.file('file', {
       size: '2mb',
       extnames: ['jpg', 'jpeg', 'png'],
     })
-
-    if(file != null && !file.isValid){
-      return response.send({message: "Le fichier n'est pas au bon format"})
-    }else{
-      if (file != null  && file.isValid) {
+    if (file != null && !file.isValid) {
+      return response.send({ message: "Le fichier n'est pas au bon format" })
+    } else {
+      if (file != null && file.isValid) {
         await file.move(Application.tmpPath('uploads'))
         console.log(file)
         url = `http://localhost:3333/uploads/${file.fileName}`
         console.log('if')
       }
-      else{
+      else {
         console.log('else')
       }
     }
-      const payload = await request.validate(StorePostValidator)
-      console.log(url)
-      payload.img_url = url
-      const post = await Post.create(payload)
-      return response.created(post)
-    }
+    const payload = await request.validate(StorePostValidator)
+    console.log(url)
+    payload.img_url = url
+    const post = await Post.create(payload)
+    return response.created(post)
+  }
 
   public async show({ response, params }: HttpContextContract) {
     const post = await Post.findOrFail(params)
@@ -73,15 +72,15 @@ export default class PostsController {
 
     const post = await Post.findOrFail(params.id)
     const payload = await request.validate(UpdatePostValidator)
-    let url =""
+    let url = ""
     const file = request.file('file', {
       size: '2mb',
       extnames: ['jpg', 'jpeg', 'png'],
     })
-    if(file != null && !file.isValid){
-      return response.send({message: "Le fichier n'est pas au bon format"})
-    }else{
-      if (file != null  && file.isValid) {
+    if (file != null && !file.isValid) {
+      return response.send({ message: "Le fichier n'est pas au bon format" })
+    } else {
+      if (file != null && file.isValid) {
         await file.move(Application.tmpPath('uploads'))
         url = `http://localhost:3333/uploads/${file.fileName}`
         payload.img_url = url
@@ -91,7 +90,7 @@ export default class PostsController {
     return response.ok(post)
   }
 
-  public async destroy({response, params }: HttpContextContract) {
+  public async destroy({ response, params }: HttpContextContract) {
 
     const post = await Post.findOrFail(params.id)
     post.delete()
